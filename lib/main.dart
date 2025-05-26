cadf1c658d9ade573afe85e51f31cf8e60661391
Hugo Henrique <mxhugoxm@gmail.com>
2025-05-26 14:31:38 -0300

    feat: Atualizar título do aplicativo para 'Sweet List'

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sweet List',
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.teal,
